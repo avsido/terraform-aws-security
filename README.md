bd35d864d32e8fcfb9011854c2d1a0fac599452e
avi.asido <avsido1988@gmail.com>
2025-05-19 12:12:13 +0300

    added module for AWS security group

---
+++ README.md
@@ -0,0 +1,2 @@
+# Create security group in AWS
+## Must have a variables.tf that specifies your IP
---
+++ security.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "terraform-ec2-sg"
+  description = "Allow SSH from my IP, HTTP/HTTPS from anywhere"
+
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["213.57.121.34/32"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-ec2-sg"
+  }
+}
